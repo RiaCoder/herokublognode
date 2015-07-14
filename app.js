5888ad603efdf5161fb660f72ae3e32941216cf6
Philip King <ilovethechiefs@gmail.com>
2015-07-14 16:42:11 -0500

    End of Lecture 28

--- app.js
+++ app.js
@@ -12,9 +12,12 @@ var multer = require('multer');
 var flash = require('connect-flash');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var posts = require('./routes/posts');
+var categories = require('./routes/categories');
 
 var app = express();
+
+app.locals.moment = require('moment');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -71,7 +74,8 @@ app.use(function(req,res,next){
 });
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/posts', posts);
+app.use('/categories', categories);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/categories.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb');
+var db = require('monk')('localhost/nodeblog');
+
+router.get('/add', function(req, res, next) {
+  res.render('addcategory', {
+    'title': "Add Category"
+  });
+});
+
+router.post('/add', function(req, res, next) {
+  // Get form values
+  var title         = req.body.title;
+
+  // Form Validation
+  req.checkBody('title', 'Title field is required').notEmpty();
+
+  // Check Errors
+  var errors = req.validationErrors();
+
+  if(errors) {
+    res.render('addcategory', {
+      'errors': errors,
+      'title': title
+    });
+  } else {
+    var categories = db.get('categories');
+
+    // Submit to DB
+    categories.insert({
+      "title": title
+    }, function(err, category){
+      if(err){
+        res.send('There was an issue submitting the category');
+      } else {
+        req.flash('success', 'Category submitted');
+        res.location('/');
+        res.redirect('/');
+      }
+    });
+  }
+});
+
+module.exports = router;
